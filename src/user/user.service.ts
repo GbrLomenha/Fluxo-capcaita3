942e7581dd2296413a11ca22418f89c7438de1b5
Gabriel Lomenha <gabriellomenha@gmail.com>
2023-07-12 21:46:09 -0300

    Não consegui fazer confirmação de usuario

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { entradaSenha } from './validar-usuario.ts';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -11,28 +12,40 @@ export class UserService {
   
   //Funções chamadas pelo controller para serem usadas com CRUD
 
-  create(createUserDto: CreateUserDto) {    //Cria um produto usando o padrão JSON com parâmetro
-    const novoUsuario = this.userRepository.create({...createUserDto}) 
-    this.userRepository.save(novoUsuario);
+
+  //Cria um usuário se for um administrador  usando o padrão JSON com parâmetro
+  create(tipo: string, createUserDto: CreateUserDto) {    
+    if(tipo != 'adm'){ //Verifica a permissão
+      throw new HttpException('This user is not an administrator',HttpStatus.BAD_REQUEST)  //Usuario não autorizado
+    }
+    const novoUsuario = this.userRepository.create({...createUserDto}) //Autorizado cria novo usuario
+    return this.userRepository.save(novoUsuario); //Salva
   }
 
-  //Mostra todos os usuarios
+  //Mostra todos os usuarios (disponível para todos os usuarios)
   findAll() {
     return this.userRepository.find();  
   }
 
-  //Mostra um usuario específico
+  //Mostra um usuario específico (disponível para todos os usuarios)
   findOne(id: number) {
     return this.userRepository.findOneBy({id});
   }
 
-  //Atualiza um usuário específico
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return this.userRepository.update({id}, {...updateUserDto});
+  //Atualiza um usuário específico se for um administrador usando o padrão JSON com parâmetro
+  update(tipo:string, id: number, updateUserDto: UpdateUserDto) {
+    if(tipo!='adm'){  //Verifica a permissão
+    throw new HttpException('This user is not an administrator',HttpStatus.BAD_REQUEST)  //Usuario não autorizado
+    }
+    return this.userRepository.update({id}, {...updateUserDto}); //Autorizado remove
   }
 
-  //Remove um usuário específico
-  remove(id: number) {
-    return this.userRepository.delete({id});
+  //Remove um usuário específico se for um administrador com permissão
+  remove(id:number, validarUsuario:interface) {
+    var senha = validarUsuario
+    if(senha != "senha")
+      throw new HttpException('This user is not an administrator',HttpStatus.BAD_REQUEST)  //Usuario não autorizado
+    
+    return this.userRepository.delete({id}); //Autorizado deleta
   }
 }
